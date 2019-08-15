2a0a9210b8cfeec2326045cc1098bea400179731
EugeneGpil <gpilligrim@gmail.com>
2019-08-15 13:40:55 +0300

    'home_page'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,27 +7,281 @@
         <title>Biemon</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,600">
+
+        <!-- FontAwesome -->
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
         <!-- Styles -->
+        <link rel="stylesheet"  type="text/css" href="/css/normalize.css">
         <link rel="stylesheet"  type="text/css" href="/css/app.css">
     </head>
     <body>
         <div class="wrapper">
+
+            <!-- Header -->
             <header class="header">
-                <div class="header__logo-container">
-                    <div class="header__logo">Biemon</div>
-                </div>
-                <div class="header__search-container">
-                    <form class="header__search">
-                        <input class="header__search-bar" name="search" placeholder="Поиск">
-                        <button class="header__search-button">
-                            <i class="fas fa-search"></i>
-                        </button>
-                    </form>
-                </div>
+                <div class="header__logo">Biemon</div>
+                <form class="header__search">
+                    <input class="header__search-bar" name="search" placeholder="Поиск">
+                    <button class="header__search-button">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </form>
             </header>
+            <!-- End of Header -->
+
+            <!-- Main Navigation Line-->
+            <nav class="navigation-line">
+                <!-- Main navigation -->
+                <div class="navigation-line__navigation">
+                    <a href="#" class="button">Рекомендации</a>
+                    <a href="#" class="button">Новое</a>
+                    <a href="#" class="button">Авторы</a>
+                    <a href="#" class="button">По категориям</a>
+                </div>
+                <!-- End of Main navigation -->
+                <!-- Login -->
+                <div class="navigation-line__login-container">
+                    <a class="navigation-line__login-button" href="#">Вход</a>
+                    <a class="navigation-line__login-button" href="#">Регистрация</a>
+                </div>
+                <!-- End of Login -->
+            </nav>
+            <!-- End of Main Navigation Line-->
+
+            <!-- Main Screen-->
+            <div class="main-screen">
+                <!-- Info -->
+                <section class="main-screen-info-container">
+                    <div class="main-screen-info">
+                        <div class="screen-headline">
+                            Тебе есть, что сказать Миру!!!
+                        </div>
+                        <a href="#" class="main-screen-info__button button">
+                            Написать статью
+                        </a>
+                    </div>
+                </section>
+                <!-- End of Info -->
+                <!-- Main screen sidebar -->
+                <aside class="sidebar">
+                    <a href="#" class="sidebar__button button">
+                        Популярное
+                    </a>
+                    <a href="#" class="article-container">
+                        <div class="article-container__image"></div>
+                        <div class="article-container__text-container">
+                            <div class="article-container__headline">
+                                Охота на снежного леопарда
+                            </div>
+                            <div class="article-container__text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a metus tristique, placerat risus ac, bibendum...
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#" class="article-container">
+                        <div class="article-container__image"></div>
+                        <div class="article-container__text-container">
+                            <div class="article-container__headline">
+                                Охота на снежного леопарда
+                            </div>
+                            <div class="article-container__text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a metus tristique, placerat risus ac, bibendum...
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#" class="article-container">
+                        <div class="article-container__image"></div>
+                        <div class="article-container__text-container">
+                            <div class="article-container__headline">
+                                Охота на снежного леопарда
+                            </div>
+                            <div class="article-container__text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a metus tristique, placerat risus ac, bibendum...
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#" class="article-container">
+                        <div class="article-container__image"></div>
+                        <div class="article-container__text-container">
+                            <div class="article-container__headline">
+                                Охота на снежного леопарда
+                            </div>
+                            <div class="article-container__text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a metus tristique, placerat risus ac, bibendum...
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#" class="article-container">
+                        <div class="article-container__image"></div>
+                        <div class="article-container__text-container">
+                            <div class="article-container__headline">
+                                Охота на снежного леопарда
+                            </div>
+                            <div class="article-container__text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a metus tristique, placerat risus ac, bibendum...
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#" class="article-container">
+                        <div class="article-container__image"></div>
+                        <div class="article-container__text-container">
+                            <div class="article-container__headline">
+                                Охота на снежного леопарда
+                            </div>
+                            <div class="article-container__text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a metus tristique, placerat risus ac, bibendum...
+                            </div>
+                        </div>
+                    </a>
+                </aside>
+                <!-- End of Main screen sidebar -->
+            </div>
+            <!-- End of Main Screen-->
+
+            <!-- Second Screen -->
+            <div class="second-screen">
+                <!-- Second Screen sidebar -->
+                <aside class="sidebar">
+                    <a href="#" class="sidebar__button button">
+                        Популярное
+                    </a>
+                    <a href="#" class="article-container">
+                        <div class="article-container__image"></div>
+                        <div class="article-container__text-container">
+                            <div class="article-container__headline">
+                                Охота на снежного леопарда
+                            </div>
+                            <div class="article-container__text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a metus tristique, placerat risus ac, bibendum...
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#" class="article-container">
+                        <div class="article-container__image"></div>
+                        <div class="article-container__text-container">
+                            <div class="article-container__headline">
+                                Охота на снежного леопарда
+                            </div>
+                            <div class="article-container__text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a metus tristique, placerat risus ac, bibendum...
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#" class="article-container">
+                        <div class="article-container__image"></div>
+                        <div class="article-container__text-container">
+                            <div class="article-container__headline">
+                                Охота на снежного леопарда
+                            </div>
+                            <div class="article-container__text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a metus tristique, placerat risus ac, bibendum...
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#" class="article-container">
+                        <div class="article-container__image"></div>
+                        <div class="article-container__text-container">
+                            <div class="article-container__headline">
+                                Охота на снежного леопарда
+                            </div>
+                            <div class="article-container__text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a metus tristique, placerat risus ac, bibendum...
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#" class="article-container">
+                        <div class="article-container__image"></div>
+                        <div class="article-container__text-container">
+                            <div class="article-container__headline">
+                                Охота на снежного леопарда
+                            </div>
+                            <div class="article-container__text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a metus tristique, placerat risus ac, bibendum...
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#" class="article-container">
+                        <div class="article-container__image"></div>
+                        <div class="article-container__text-container">
+                            <div class="article-container__headline">
+                                Охота на снежного леопарда
+                            </div>
+                            <div class="article-container__text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a metus tristique, placerat risus ac, bibendum...
+                            </div>
+                        </div>
+                    </a>
+                </aside>
+                <!-- End of Second Screen Sidebar -->
+                <!-- Second Screen Info -->
+                <section class="second-screen-info-container">
+                    <div class="second-screen-info">
+                        <div class="screen-headline">
+                            Их читают!
+                        </div>
+                        <div class="authors-container">
+                            <a href="#" class="article-container">
+                                <div class="article-container__image"></div>
+                                <div class="article-container__text-container">
+                                    <div class="article-container__headline">
+                                        Иванов Иван Иванович
+                                    </div>
+                                    <div class="article-container__text">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a metus tristique, placerat risus ac, bibendum...
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="#" class="article-container">
+                                <div class="article-container__image"></div>
+                                <div class="article-container__text-container">
+                                    <div class="article-container__headline">
+                                        Иванов Иван Иванович
+                                    </div>
+                                    <div class="article-container__text">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a metus tristique, placerat risus ac, bibendum...
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="#" class="article-container">
+                                <div class="article-container__image"></div>
+                                <div class="article-container__text-container">
+                                    <div class="article-container__headline">
+                                        Иванов Иван Иванович
+                                    </div>
+                                    <div class="article-container__text">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a metus tristique, placerat risus ac, bibendum...
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="#" class="article-container">
+                                <div class="article-container__image"></div>
+                                <div class="article-container__text-container">
+                                    <div class="article-container__headline">
+                                        Иванов Иван Иванович
+                                    </div>
+                                    <div class="article-container__text">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a metus tristique, placerat risus ac, bibendum...
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <a class="second-screen-info__button button">Зарегистрироваться</a>
+                    </div>
+                </section>
+                <!-- End of Second Screen Info -->
+            </div>
+            <!-- End of Second Screen -->
+
+            <!-- Footer -->
+            <footer>
+                <div class="button">Инфо</div>
+                <div class="footer__year">2019</div>
+                <div class="button">Наверх<i class="fas fa-arrow-up"></i></div>
+            </footer>
+            <!-- End of Footer -->
+
         </div>
     </body>
 </html>
